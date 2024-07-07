b3d594196f825831332443b26d4ce8d25577ce3b
未知可行 <14187349+xcanwin@users.noreply.github.com>
2024-07-08 02:58:42 +0800

    更新描述

--- EasyZHIHU.user.js
+++ EasyZHIHU.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EasyZHIHU
-// @description  这是一款促进知乎极致简洁和高效的插件。免费共享大量创新功能，如：净化页面、免登录等。让我们的学习体验无比简洁、专注、高效、畅快。
-// @version      3.0
+// @description  这是一款促进知乎极致简洁和高效的插件。免费共享大量创新功能，如：净化页面、免登录、展示全屏等。让我们的学习体验无比简洁、专注、高效、畅快。
+// @version      4.0
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/EasyZHIHU/
 // @supportURL   https://github.com/xcanwin/EasyZHIHU/
