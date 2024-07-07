55078c4bc7e6dfe689162fdcd9daf52bd40433f6
未知可行 <14187349+xcanwin@users.noreply.github.com>
2024-07-08 04:04:43 +0800

    净化页面背景

--- EasyZHIHU.user.js
+++ EasyZHIHU.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EasyZHIHU
 // @description  这是一款促进知乎极致简洁和高效的插件。免费共享大量创新功能，如：净化页面、免登录、展示全屏等。让我们的学习体验无比简洁、专注、高效、畅快。
-// @version      4.0
+// @version      5.0
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/EasyZHIHU/
 // @supportURL   https://github.com/xcanwin/EasyZHIHU/
@@ -26,7 +26,7 @@ body {
 
 /*净化页面*/
 .Sticky {
-    display: none !important;
+    display: none;
 }
 
 /*调整标题*/
@@ -36,14 +36,14 @@ body {
     font-size: 51.5px;
 }
 .aria-question-header .aria-question-text .RichText p {
-    margin: unset !important;
+    margin: unset;
 }
 
 /*调整提问框*/
 .aria-question-text {
 	background: #f6f6f6;
-	padding: 32px 48px 32px 48px !important;
-    margin-top: 32px !important;
+	padding: 32px 48px 32px 48px;
+    margin-top: 32px;
 }
 
 /*展示全屏*/
@@ -54,9 +54,30 @@ body {
     margin-top: 48px;
 }
 
+/*正文背景*/
+.aria-primary-color-style.aria-secondary-background {
+    background: unset;
+}
+
 /*正文的图片居中*/
 .css-1ygg4xu img.content_image, .css-1ygg4xu img.origin_image {
     width: unset !important;
+}
+
+/*正文分割线*/
+.aria-answer-count {
+	border-bottom: 3px solid;
+	border-color: #bdbdbd;
+	margin: unset;
+	padding: 20px 0;
+}
+.List-item + .List-item::after {
+    left: -40px;
+    right: -40px;
+    height: 3px;
+    background: linear-gradient(to right, #ffffff, #bdbdbd, #ffffff);
+    border-bottom: unset;
+    border-color: unset;
 }
 
 `;
