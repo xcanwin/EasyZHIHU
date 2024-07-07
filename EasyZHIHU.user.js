09655713e0030efa94079d1fbc9d0b3f0c4c8143
未知可行 <14187349+xcanwin@users.noreply.github.com>
2024-07-08 05:04:59 +0800

    恢复链接，提高净化能力

--- EasyZHIHU.user.js
+++ EasyZHIHU.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EasyZHIHU
 // @description  这是一款促进知乎极致简洁和高效的插件。免费共享大量创新功能，如：净化页面、免登录、展示全屏等。让我们的学习体验无比简洁、专注、高效、畅快。
-// @version      7.0
+// @version      8.0
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/EasyZHIHU/
 // @supportURL   https://github.com/xcanwin/EasyZHIHU/
@@ -28,7 +28,7 @@ body {
 .Sticky /*隐藏[置顶的][顶部的]菜单栏*/,
 .CornerButtons /*隐藏[底部的]回到顶部按钮*/
 {
-    display: none;
+    display: none !important;
 }
 
 /*调整标题*/
@@ -50,15 +50,15 @@ body {
 
 /*展示全屏*/
 .aria-centered-wrapper {
-    width: 78%;
+    width: 78% !important;
 }
 .aria-answer-wrapper {
-    padding: unset;
+    padding: unset !important;
 }
 
 /*调整正文背景*/
 .aria-primary-color-style.aria-secondary-background {
-    background: unset;
+    background: unset !important;
 }
 
 /*正文的图片居中*/
@@ -68,15 +68,15 @@ body {
 
 /*调整正文的回答数量栏*/
 .aria-answer-count {
-    border-bottom: 4px solid;
-    border-color: #bdbdbd;
-    margin: unset;
+    border-bottom: 4px solid !important;
+    border-color: #bdbdbd !important;
+    margin: unset !important;
 }
 
 /*调整正文分割线*/
 .List-item + .List-item::after {
-    left: -20px;
-    right: -20px;
+    left: -20px !important;
+    right: -20px !important;
     height: 3px;
     background: #bdbdbd;
     border-bottom: unset;
@@ -85,15 +85,19 @@ body {
 
 /*净化底部*/
 .aria-page-wrapper {
-    padding-bottom: unset;
+    padding-bottom: unset !important;
 }
 
 `;
 
-    if (location.href.match(/https:\/\/www.zhihu.com\/question\/(\d+)(?:\/answer\/(\d+))?/)) {
-        location.href = location.href.replace("www.zhihu.com", "www.zhihu.com/aria");
-    } else if (location.href.match(/https:\/\/www.zhihu.com\/aria\/question\/(\d+)(?:\/answer\/(\d+))?/)) {
+    const current_url = location.href;
+    if (current_url.match(/https:\/\/www.zhihu.com\/question\/(\d+)(?:\/answer\/(\d+))?/)) {
+        location.href = current_url.replace("www.zhihu.com", "www.zhihu.com/aria");
+    } else if (current_url.match(/https:\/\/www.zhihu.com\/aria\/question\/(\d+)(?:\/answer\/(\d+))?/)) {
         GM_addStyle(purify_style_pc);
+        window.onload = function() {
+            window.history.replaceState(null, '', current_url.replace("www.zhihu.com/aria", "www.zhihu.com"));
+        }
     }
 
 })();
